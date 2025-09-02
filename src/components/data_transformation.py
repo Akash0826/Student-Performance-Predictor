72ce56ccc8f95fc278a7596ce3b5680dda9ed944
AKASH M <akash.au19@gmail.com>
2025-09-02 13:02:27 +0530

    Data Ingestion

---
+++ src/components/data_transformation.py
@@ -0,0 +1,31 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path: str = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data transformation
+        '''
+        try:
+            numerical_columns = ['math score', 'reading score', 'writing score']
+            categorical_columns = [''
+            ]
+
+        except Exception as e:
+            raise CustomException(e, sys)
